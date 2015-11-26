faff2ac739580de688e3b134d873749ad05baee4
Evan Hahn <me@evanhahn.com>
2015-11-26 15:14:40 -0500

    compact

---
+++ test/compact.js
@@ -0,0 +1,39 @@
+var compact = require('../compact');
+
+describe('compact', function () {
+  it('returns an empty array when passed an empty array', function () {
+    expect(compact([])).to.deep.equal([]);
+  });
+  
+  it('keeps the original array intact', function () {
+    var arr = [0, 1, 2];
+    expect(compact(arr)).not.to.equal(arr);
+    expect(arr).to.deep.equal([0, 1, 2]);
+  });
+  
+  it('returns the array with falsy values removed', function () {
+    var arr = [
+      'foo',
+      '',
+      false,
+      true,
+      10,
+      0,
+      -0,
+      undefined,
+      1,
+      null,
+      2,
+      0 / 0,
+      3
+    ];
+    expect(compact(arr)).to.deep.equal([
+      'foo',
+      true,
+      10,
+      1,
+      2,
+      3
+    ]);
+  });
+});
