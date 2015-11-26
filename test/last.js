ed39ddc7e2e37ad3d0c187e1649eec698ec02ccb
Evan Hahn <me@evanhahn.com>
2015-11-26 11:29:11 -0800

    last

---
+++ test/last.js
@@ -0,0 +1,41 @@
+var last = require('../last');
+
+describe('last', function () {
+  describe('arrays', function () {
+    it('returns the last element if there is one', function () {
+      expect(last(['foo'])).to.equal('foo');
+    });
+    
+    it('returns undefined when passed an empty array', function () {
+      expect(last([])).to.equal(undefined);
+    });
+  });
+  
+  describe('arraylikes', function () {
+    it('returns the last element if there is one', function () {
+      expect(last({
+        0: 'foo',
+        1: 'bar',
+        length: 2
+      })).to.equal('bar');
+    });
+    
+    it('returns undefined when passed an empty string', function () {
+      expect(last({})).to.equal(undefined);
+    });
+    
+    it('returns undefined when passed an object with a 0 length', function () {
+      expect(last({length: 0})).to.equal(undefined);
+    });
+  });
+  
+  describe('strings', function () {
+    it('returns the last character if there is one', function () {
+      expect(last('foop')).to.equal('p');
+    });
+    
+    it('returns undefined when passed an empty string', function () {
+      expect(last('')).to.equal(undefined);
+    });
+  });
+});
