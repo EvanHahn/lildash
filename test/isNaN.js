480f4d76f6c94ba3e50017faed353e7cba2eda11
Evan Hahn <me@evanhahn.com>
2015-11-26 11:15:24 -0800

    isNaN

---
+++ test/isNaN.js
@@ -0,0 +1,26 @@
+var isnan = require('../isnan');
+
+describe('isNaN', function () {
+  describe('returns true for', function () {
+    it('NaN', function () {
+      expect(isnan(0 / 0)).to.be.true;
+    });
+  });
+  
+  describe('returns false for', function () {
+    it('numbers', function () {
+      expect(isnan(0)).to.be.false;
+      expect(isnan(-0)).to.be.false;
+      expect(isnan(1)).to.be.false;
+      expect(isnan(-1)).to.be.false;
+      expect(isnan(1.2)).to.be.false;
+      expect(isnan(-1.2)).to.be.false;
+    });
+    
+    it('strings', function () {
+      expect(isnan('')).to.be.false;
+      expect(isnan(' ')).to.be.false;
+      expect(isnan('hello')).to.be.false;
+    });
+  });
+});
