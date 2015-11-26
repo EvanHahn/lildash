da3ac9d2212ca3d13ea0dafb6345ebb21e842ce7
Evan Hahn <me@evanhahn.com>
2015-11-26 15:12:42 -0500

    identity

---
+++ test/identity.js
@@ -0,0 +1,11 @@
+var identity = require('../identity');
+
+describe('identity', function () {
+  it('returns undefined when passed no arguments', function () {
+    expect(identity()).to.equal(undefined);
+  });
+
+  it('returns the first argument passed', function () {
+    expect(identity(1, 2, 3)).to.equal(1);
+  });
+});
