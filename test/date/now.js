7c464c35c44864d5fd4cafef1ed70fa588f7ad5d
Evan Hahn <me@evanhahn.com>
2015-11-27 18:56:59 -0500

    Make date/now tests a little stronger

--- test/date/now.js
+++ test/date/now.js
@@ -2,6 +2,6 @@ var now = require('../../date/now');
 
 describe('date/now', function () {
   it('returns the current time', function () {
-    expect(now()).to.equal(new Date().getTime());
+    expect(now()).to.be.closeTo(new Date().getTime(), 10);
   });
 });
