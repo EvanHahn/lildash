36ebc0cefc929159cf379fc5b8e7f8b0f9035e29
Evan Hahn <me@evanhahn.com>
2015-11-27 19:43:55 -0500

    Add JSDoc for date methods

--- date/clone.js
+++ date/clone.js
@@ -1,3 +1,15 @@
+/**
+Clone a date.
+
+@module date/clone
+@param {Date} date The Date object to clone.
+@returns {Date} The cloned date.
+@example
+var clone = require('lildash/date/clone');
+var now = new Date();
+clone(now) === now;                      // => false
+clone(now).getTime() === now.getTime();  // => true
+*/
 module.exports = function (date) {
   return new Date(date.getTime());
 };
--- date/now.js
+++ date/now.js
@@ -1,3 +1,14 @@
+/**
+Return the number of milliseconds since the Unix epoch (the first moment of 1970).
+
+@module date/now
+@returns {number} The number of milliseconds since the Unix epoch.
+@example
+var now = require('lildash/date/now');
+now();  // => 1448671388273
+// ...time passes...
+now();  // => 1448671389644
+*/
 module.exports = function () {
   return new Date().getTime();
 };
