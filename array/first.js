cc9948d538c064888d83e0eafd9d3cef717b35a0
Evan Hahn <me@evanhahn.com>
2015-11-28 09:46:24 -0500

    Some tweaks to array/{first,last}

--- array/first.js
+++ array/first.js
@@ -1,9 +1,9 @@
 /**
-Return the first element in an array. Returns undefined if given an empty array.
+Return the first element in an array. Returns `undefined` if given an empty array.
 
 @module array/first
 @param {Array} arr The array to extract from.
-@returns {} The first element.
+@returns {*} The first element.
 @example
 var first = require('lildash/array/first');
 first([1, 2, 3]);  // => 1
--- array/last.js
+++ array/last.js
@@ -1,9 +1,9 @@
 /**
-Return the last element in an array. Returns undefined if given an empty array.
+Return the last element in an array. Returns `undefined` if given an empty array.
 
 @module array/last
 @param {Array} arr The array to extract from.
-@returns {} The last element.
+@returns {*} The last element.
 @example
 var last = require('lildash/array/last');
 last([1, 2, 3]);  // => 3
