5b15efa2a01ccdb6ed4c92128df362e585bbd868
Evan Hahn <me@evanhahn.com>
2015-11-29 11:33:06 -0500

    Add JSDoc documentation to string/startsWith

--- string/startswith.js
+++ string/startswith.js
@@ -1,3 +1,18 @@
+/**
+Does a string start with another string?
+
+@module string/startsWith
+@param {string} str The string to search through.
+@param {string} query The string to find in `str`.
+@param {number} [position=0] The index to start the search. Defaults to the beginning of the string.
+@returns {bool} Does the string start with another string?
+@example
+var startsWith = require('lildash/string/startswith');
+startsWith('food', 'foo');   // => true
+startsWith('cake', 'foo');   // => false
+startsWith('cake', 'k');     // => false
+startsWith('cake', 'k', 2);  // => true
+*/
 module.exports = function (str, query, position) {
   return str.substr(position, query.length) === query;
 };
