f19fc6f263902f17d298160699c467568b0c4f17
Alex Booker <alexbooker@fastmail.im>
2015-12-05 18:55:22 +0000

    added chars arg to string/trimRight

--- string/trimRight.js
+++ string/trimRight.js
@@ -1,3 +1,7 @@
-module.exports = function (str) {
-  return str.replace(/( [ ]*$)/g, '');
+module.exports = function (str, chars) {
+
+  var chars = chars || ' ';
+  var pattern = new RegExp('([' + chars + ']*$)', 'g');
+
+  return str.replace(pattern, '');
 };
--- test/string/trimRight.js
+++ test/string/trimRight.js
@@ -5,17 +5,29 @@ describe('trimRight', function () {
     expect(trimRight('')).to.be.a('string');
   });
 
-  var testData = [
+  [
     { input: 'foo   ', expected: 'foo' },
     { input: 'bar ', expected: 'bar' },
     { input: 'foo bar ', expected: 'foo bar' },
     { input: '   baz   ', expected: '   baz' },
     { input: '', expected: '' },
     { input: '  ', expected: '' },
-  ];
-  testData.forEach(function(data) {
+  ].forEach(function(data) {
     it('returns correct result', function () {
       expect(trimRight(data.input)).to.equal(data.expected);
     });
   });
+
+  [
+    { input: 'foo0', chars: '0', expected: 'foo' },
+    { input: 'bar', chars: '', expected: 'bar' },
+    { input: '', chars: '', expected: '' },
+    { input: 'bar123', chars: '2', expected: 'bar123' },
+    { input: '-_-abc-_-', chars: '_-', expected: '-_-abc' },
+  ].forEach(function(data) {
+    it('with chars returns correct result', function () {
+      expect(trimRight(data.input, data.chars)).to.equal(data.expected);
+    });
+  });
+
 });
