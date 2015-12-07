e1002789060b8fbfee8cbf1814ad34d8da8d8dc3
Alex Booker <alexbooker@fastmail.im>
2015-12-07 00:57:14 +0000

    added more examples to string/trimRight docs

--- string/trimRight.js
+++ string/trimRight.js
@@ -7,6 +7,8 @@ Removes trailing chars from the right end of a string.
 @example
 var trimRight = require('lildash/string/trimRight');
 trimRight('foo  ');  // => 'foo'
+trimRight('bar\t');  // => 'bar'
+trimRight('foo\t  ');  // => 'foo'
 */
 module.exports = function (str, chars) {
   return str.replace(/\s*$/g, '');
