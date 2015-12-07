8e59d91fd9ca72fb71a320ff5d1039846b311cad
Alex Booker <alexbooker@fastmail.im>
2015-12-07 00:48:55 +0000

    removed char argument from string/trimRight

--- string/trimRight.js
+++ string/trimRight.js
@@ -3,17 +3,11 @@ Removes trailing chars from the right end of a string.
 
 @module string/trimRight
 @param {string} str The string to remove trailing chars from.
-@param {string} [chars=' '] The chars to remove.
 @returns {string} The given string without trailing chars.
 @example
 var trimRight = require('lildash/string/trimRight');
 trimRight('foo  ');  // => 'foo'
-trimRight('_foo_', '_');  // => '_foo'
-trimRight('!--foo--!', '!-');  // => '!--foo'
 */
 module.exports = function (str, chars) {
-  chars = chars || ' ';
-  var pattern = new RegExp('([' + chars + ']*$)', 'g');
-
-  return str.replace(pattern, '');
+  return str.replace(/[ ]*$/g, '');
 };
--- test/string/trimRight.js
+++ test/string/trimRight.js
@@ -5,28 +5,17 @@ describe('trimRight', function () {
     expect(trimRight('')).to.be.a('string');
   });
 
-  [
+  var cases = [
     { input: 'foo   ', expected: 'foo' },
     { input: 'bar ', expected: 'bar' },
     { input: 'foo bar ', expected: 'foo bar' },
     { input: '   baz   ', expected: '   baz' },
     { input: '', expected: '' },
     { input: '  ', expected: '' }
-  ].forEach(function (data) {
+  ];
+  cases.forEach(function (data) {
     it('returns correct result', function () {
       expect(trimRight(data.input)).to.equal(data.expected);
-    });
-  });
-
-  [
-    { input: 'foo0', chars: '0', expected: 'foo' },
-    { input: 'bar', chars: '', expected: 'bar' },
-    { input: '', chars: '', expected: '' },
-    { input: 'bar123', chars: '2', expected: 'bar123' },
-    { input: '-_-abc-_-', chars: '_-', expected: '-_-abc' }
-  ].forEach(function (data) {
-    it('with chars returns correct result', function () {
-      expect(trimRight(data.input, data.chars)).to.equal(data.expected);
     });
   });
 });
