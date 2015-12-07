bb1c05d99209eed8f27c3c9b848b353cd0513e7b
Alex Booker <alexbooker@fastmail.im>
2015-12-07 00:56:40 +0000

    improved test coverage and consistency

--- test/string/trimRight.js
+++ test/string/trimRight.js
@@ -5,18 +5,19 @@ describe('trimRight', function () {
     expect(trimRight('')).to.be.a('string');
   });
 
-  var cases = [
+  var testData = [
     { input: 'foo   ', expected: 'foo' },
     { input: 'bar ', expected: 'bar' },
     { input: 'foo bar ', expected: 'foo bar' },
     { input: 'foo\t', expected: 'foo' },
+    { input: 'foo\t  ', expected: 'foo' },
     { input: 'bar\t\t', expected: 'bar' },
     { input: '   baz   ', expected: '   baz' },
     { input: '', expected: '' },
     { input: '  ', expected: '' }
   ];
-  cases.forEach(function (data) {
-    it('returns correct result', function () {
+  testData.forEach(function (data) {
+    it('returns correct result for ' + data.input, function () {
       expect(trimRight(data.input)).to.equal(data.expected);
     });
   });
