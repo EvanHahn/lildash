e6ccd909049c6bcd26874f00977953aa71cea027
Evan Hahn <me@evanhahn.com>
2015-12-08 16:34:02 -0800

    Fix some errors in number/isFinite docs
    Closes #217.

--- number/isfinite.js
+++ number/isfinite.js
@@ -2,10 +2,10 @@
 Is a number finite?
 
 @module number/isfinite
-@param {Number} num The number to check.
+@param {number} num The number to check.
 @returns {bool} Is `num` positive or negative `Infinity`?
 @example
-var isFinite = require('lildash/object/isfinite');
+var isFinite = require('lildash/number/isfinite');
 isFinite(123);     // => true
 isFinite(1 / 0);   // => false
 isFinite(-1 / 0);  // => false
